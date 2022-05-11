4e47af8d5096e998659a8a611f8081e6bec6bed0
Seifeddine BOUAZZA <sbouazza@vermeg.com>
2022-05-11 22:00:39 +0100

    feat(expressjs): add user controller

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,4 @@
-const User = (id, firstName, lastName, email, age) => {
+const User = function (id, firstName, lastName, email, age) {
   this.id = id;
   this.firstName = firstName;
   this.lastName = lastName;
@@ -6,7 +6,7 @@ const User = (id, firstName, lastName, email, age) => {
   this.age = age;
 };
 
-User.fromObject = (json) => {
+User.fromObject = function (json) {
   return new User(json.id, json.firstName, json.lastName, json.email, json.age);
 };
 module.exports = { User };
